3cc8864309d608ac47ba7470ae05ec0013e2b427
Mitchwatts93 <31891580+Mitchwatts93@users.noreply.github.com>
2019-10-18 11:13:11 +0100

    first

---
+++ utils.py
@@ -0,0 +1,62 @@
+from visualisation import return_html
+
+def eval_from_input(eval_text):
+    """Generate output from a sequence"""
+
+    ### replace this bit with generating an output instead
+    html_result = generate_sentiment_html(eval_text)
+
+    # Formatting in html
+    html = ''
+    html = addContent(html, header(
+        'Sentiment Analysis', color='#FE9023'))
+    html = addContent(html, box(html_result))
+    return f'<div>{html}</div>'
+
+
+def generate_sentiment_html(eval_text):
+    sentiment, confidence, model = get_sentiment(eval_text)
+    html = return_html(sentiment, confidence, model, eval_text)
+    return html
+
+def get_sentiment(eval_text):
+    from flair.data import Sentence
+    model = load_model()
+    S = Sentence(eval_text)
+    sentiment, confidence = eval_model(model, S)
+    # should also do the explaining in here but will need to change name
+    return sentiment, confidence, model  # this will be fixed in the future
+
+
+def eval_model(model, sentence_obj):
+    model.predict(sentence_obj)
+    result = sentence_obj.labels[0]  # assuming theres only one sentence!
+    sentiment, confidence = result.value, result.score
+    return sentiment, confidence
+
+
+def load_model():
+    """Load in the pre-trained model"""
+    from flair.models import TextClassifier
+    model = TextClassifier.load('./en-sentiment')
+    return model
+
+
+def header(text, color='black'):
+    """Create an HTML header"""
+    raw_html = f'<h1 style="margin-top:12px;color: {color};font-size:54px"><center>' + str(
+        text) + '</center></h1>'
+    return raw_html
+
+
+def box(text):
+    """Create an HTML box of text"""
+    raw_html = '<div style="border-bottom:1px inset black;border-top:1px inset black;padding:8px;font-size: 28px;">' + str(
+        text) + '</div>'
+    return raw_html
+
+
+def addContent(old_html, raw_html):
+    """Add html content together"""
+    old_html += raw_html
+    return old_html
---
+++ visualisation.py
@@ -0,0 +1,90 @@
+from flair.models import TextClassifier
+from flair.data import Sentence
+from tqdm import tqdm as tqdm
+import numpy as np
+from functools import partial
+
+
+class FlairExplainer:
+
+    def __init__(self, clf):
+        self.classifier = clf
+
+    def predict(self, texts):
+        docs = [Sentence(text) for text in texts]
+        docs[:] = map(partial(self.classifier.predict, multi_class_prob=True), tqdm(docs))
+        labels = [[x.value for x in doc[0].labels] for doc in docs]#assumes only one sentence per doc
+        probs = [[x.score for x in doc[0].labels] for doc in docs]
+        probs = np.array(probs)   # Convert probabilities to Numpy array
+
+        # For each prediction, sort the probability scores in the same order for all texts
+        result = []
+        for label, prob in zip(labels, probs):
+            order = np.argsort(np.array(label))[::-1] # negative positive
+            result.append(prob[order])
+        return np.array(result)
+
+
+def explainer(clf, text):
+    """Run LIME explainer on provided classifier"""
+    from lime.lime_text import LimeTextExplainer
+
+    model = FlairExplainer(clf)
+    predictor = model.predict
+
+    explainer = LimeTextExplainer(
+        split_expression=lambda x: x.split(),
+        bow=False,
+        class_names=["POSITIVE", "NEGATIVE"]
+    )
+    exp = explainer.explain_instance(
+        text,
+        classifier_fn=predictor,
+        num_features=20,
+        num_samples=100
+    )
+    return exp
+
+def visualise_sentiments(data):
+    from matplotlib.figure import Figure
+    import matplotlib.pyplot as plt
+    from matplotlib import cm
+    import seaborn as sns
+    import pandas as pd
+    cmap = cm.coolwarm.reversed()
+    fig = plt.gcf()
+    figsize = fig.get_size_inches()
+    fig.set_size_inches(figsize[0]*3, figsize[1]*2) # make it wider than default - a bit of hacky solution
+    ax = sns.heatmap(pd.DataFrame(data).set_index("Sentence").T, center=0, annot=True, cmap=cmap)
+    fig = ax.get_figure()
+    fig.patch.set_facecolor('white')
+    fig.patch.set_alpha(0.3)
+    plt.tight_layout()
+    del ax
+    return fig
+
+def return_html(sentiment, confidence, model, eval_text):
+    sentiment = (-1)**(sentiment=="NEGATIVE") # a bit hacky so sort that out. is 1 if positive and -1 if neg
+
+    exp = explainer(model, eval_text)
+    keys = [key[0] for key in exp.as_map()[1]] # get the ranked order of the keys
+    zipped_words = zip(list(exp.as_list()), keys) # get a list of tuples ((word, score), key)
+    scores = [x for x, _ in sorted(zipped_words, key=lambda pair: pair[1])] # sort by the key so now in original order
+
+    import matplotlib.pyplot as plt
+    fig = visualise_sentiments({
+        "Sentence": ["SENTENCE"] + [score[0] for score in scores],
+        "Sentiment": [confidence * sentiment] + [score[1] for score in scores],
+    })
+    html = html_from_fig(fig)
+    plt.clf()
+    return html
+
+def html_from_fig(fig):
+    from io import BytesIO
+    import base64
+    buf = BytesIO()
+    fig.savefig(buf, format="png", )
+    # Embed the result in the html output.
+    data = base64.b64encode(buf.getbuffer()).decode("ascii") # see img scaling setup for html?
+    return f"<img src='data:image/png;base64,{data}'; style='height: 100%; width: 100%; object-fit: contain'/>" # f"<img src='data:image/png;base64,{data}'/>"
